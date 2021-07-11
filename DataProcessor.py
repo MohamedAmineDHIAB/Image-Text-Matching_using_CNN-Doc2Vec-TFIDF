df8f80b431a516d0482adfbf11b85f662d72653d
MohamedAmineDHIAB <85687148+MohamedAmineDHIAB@users.noreply.github.com>
2021-07-11 17:48:35 +0200

    Update DataProcessor.py

--- DataProcessor.py
+++ DataProcessor.py
@@ -12,7 +12,7 @@ import spacy
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfVectorizer
 
-from gensim.models.doc2vec import Doc2Vec, TaggedDocument
+
 from argparse import ArgumentParser
 
 import DataCollector as dc
@@ -99,33 +99,7 @@ def fit_tfidf(corpus : pd.DataFrame, test_corpus : pd.DataFrame = None, max_feat
 
     return tfidf_vectorizer, tfidf_train_corpus 
 
-def fit_doc2vec(corpus : pd.DataFrame, test_corpus : pd.DataFrame = None, 
-                epochs : int = 100, vector_size : int = 256, alpha : float = 0.025, min_count : int = 1):
 
-    corpus = [custom_tokenizer(doc) for doc in corpus.values]
-    tagged_corpus = [TaggedDocument(words=doc, tags=[i]) for i, doc in enumerate(corpus)]
-
-    model_d2v = Doc2Vec(vector_size = vector_size, alpha = alpha, min_count = min_count)
-    model_d2v.build_vocab(tagged_corpus)
-
-    for epoch in range(epochs):
-        model_d2v.train(tagged_corpus, total_examples = model_d2v.corpus_count, epochs = model_d2v.epochs)
-
-    doc2vec_train_corpus = np.zeros((len(corpus), vector_size))
-    for i in range(len(corpus)):
-        doc2vec_train_corpus[i] = model_d2v.dv[i]
-
-    if test_corpus is not None:
-
-        test_corpus = [custom_tokenizer(doc) for doc in test_corpus.values]
-
-        doc2vec_test_corpus = np.zeros((len(test_corpus), vector_size))
-        for i in range(len(test_corpus)):
-            doc2vec_test_corpus[i] = model_d2v.infer_vector(test_corpus[i])
-
-        return model_d2v, doc2vec_train_corpus, doc2vec_test_corpus
-
-    return model_d2v, doc2vec_train_corpus
 
 def calculate_most_similar(vectors : pd.DataFrame, target_vectors : pd.DataFrame, index_vector : pd.DataFrame = None):
 
@@ -229,9 +203,8 @@ if __name__ == "__main__":
 
     data_text, data_img = dc.get_test_data(path.join(data_dir, "03_data.tsv"), path.join(data_dir, "03_label.tsv"), path.join(img_dir, "03"))
 
-    [dst_file_bow, dst_file_tfidf, dst_file_doc2vec] = [path.join(dst_text_features, 'bow_data.npy'), 
-                                                        path.join(dst_text_features, 'tfidf_data.npy'),
-                                                        path.join(dst_text_features, 'doc2vec_data.npy')]
+    [dst_file_bow, dst_file_tfidf] = [path.join(dst_text_features, 'bow_data.npy'), 
+                                                        path.join(dst_text_features, 'tfidf_data.npy')]
 
     
     # Generate Image-Features (VGG19, ResNet152, DenseNet201) for 'Training'-data:
@@ -246,22 +219,8 @@ if __name__ == "__main__":
     save_results([vgg_fc1_features], dst_file_vgg_fc1) 
     print("VGG19 - 1")
 
-    """
-    vgg_fc2_features = transform_cnn(images.copy(), pretrained_model = VGG19, include_top = True, layer = "fc2")  
-    save_results([vgg_fc2_features], dst_file_vgg_fc2)
-    print("VGG19 - 2")
 
-    resnet_features = transform_cnn(images.copy(), pretrained_model = ResNet152V2, include_top = True, layer = "avg_pool")
-    save_results([resnet_features], dst_file_resnet)
-    print("ResNet")
-
-    densenet_features = transform_cnn(images.copy(), pretrained_model = DenseNet201, include_top = True, layer = "avg_pool") 
-    save_results([densenet_features], dst_file_densenet)
-    print("DenseNet") 
-    """
-    
-
-    # Generate Text-Features (BOW, TFIDF, DOC2VEC) for 'Training' and 'Test'-data:
+    # Generate Text-Features (BOW, TFIDF) for 'Training' and 'Test'-data:
        
     # Set Text-Feature Parameters:
     vector_size, epochs = 4096, 100
@@ -272,29 +231,6 @@ if __name__ == "__main__":
     _, train_tfidf, test_tfidf = fit_tfidf(adata["text"], test_corpus = data_text["text"], max_features = vector_size)
     save_results([train_tfidf, test_tfidf], dst_file_tfidf)
 
-    _, train_doc2vec, test_doc2vec = fit_doc2vec(adata["text"], test_corpus = data_text["text"], epochs = epochs, vector_size = vector_size)
-    save_results([train_doc2vec, test_doc2vec], dst_file_doc2vec)
+   
     
 
-    # Compute the most similar article (text) from the 'Training'-articles for each 'Test'-article:
-    """
-    result_bow, result_tfidf, result_doc2vec = load_results(dst_file_bow), load_results(dst_file_tfidf), load_results(dst_file_doc2vec)
-
-    data_text, data_img = dc.get_test_data(path.join(data_dir, "03_data.tsv"), path.join(data_dir, "03_label.tsv"), path.join(img_dir, "03"))
-
-    adata["bow"], adata["tfidf"], adata["doc2vec"] = list(result_bow[0]), list(result_tfidf[0]), list(result_doc2vec[0])
-    data_text["bow"], data_text["tfidf"], data_text["doc2vec"] = list(result_bow[1]), list(result_tfidf[1]), list(result_doc2vec[1])
-
-    bow_most_similar, bow_cosine_similarity = calculate_most_similar(data_text["bow"], adata["bow"], adata["aid"])
-    tfidf_most_similar, tfidf_cosine_similarity = calculate_most_similar(data_text["tfidf"], adata["tfidf"], adata["aid"])
-    doc2vec_most_similar, doc2vec_cosine_similarity = calculate_most_similar(data_text["doc2vec"], adata["doc2vec"], adata["aid"])
-
-    data_text["bow_most_similar"], data_text["bow_cosine_similarity"] = bow_most_similar, bow_cosine_similarity
-    data_text["tfidf_most_similar"], data_text["tfidf_cosine_similarity"] = tfidf_most_similar, tfidf_cosine_similarity
-    data_text["doc2vec_most_similar"], data_text["doc2vec_cosine_similarity"] = doc2vec_most_similar, doc2vec_cosine_similarity
-
-    del data_text["bow"]
-    del data_text["tfidf"]
-    del data_text["doc2vec"]
-    data_text.to_csv(path.join(data_dir, "03_data_processed.tsv"), sep='\t')   
-    """
